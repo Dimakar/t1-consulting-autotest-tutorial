c67bd4b17b6d49fbca8ffa5db163f1a1c134798d
dimakar <makarovskii@phystech.edu>
2022-09-19 14:42:25 +0300

    1. maven lesson

---
+++ src/main/java/ru/t1consulting/App.java
@@ -0,0 +1,14 @@
+package ru.t1consulting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
